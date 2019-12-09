7a82632d623953b0a16949ba2221d23ca4cf7cb3
khazaddoom <31121986g@gmail.com>
2019-12-09 17:38:08 +0530

    minor logic correction

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,91 +7,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  value: number;
+  previousOperand = '';
+  currentOperand = '';
+  operation = '';
 
-  path: string = '';
-
-
-  side: [number, number];
-  index: number;
-
-  operateOn = null;
+  displayValue = '';
 
   constructor() { }
 
-  ngOnInit() {
-    this.initialize();
-  }
+  ngOnInit() {}
 
-  handleClick(input, operator) {
+  handleClick(input, operatorOrNumber) {
 debugger;
-    this.path = this.path.concat(input);
-
-    if (operator) {
-
-      if(input === 'AC') this.initialize();
-      
-      if(this.index === 1) {
-        // calculate(this.operateOn)
-        this.value = this.calculate(this.operateOn);
-        
-        if(input !== '=')
-          this.operateOn = input;
-        else {
-          this.index = 1;
-        }
-
+    if (operatorOrNumber) {
+      if(this.previousOperand !== '' && this.currentOperand !== '') {
+        this.calculate();
+        this.operation = input;
+        this.currentOperand = '';
+      }
+      else if(input === '=') {
+        this.calculate();
+        this.previousOperand = this.displayValue;
+        this.currentOperand = '';
       } else {
-        this.operateOn = input;
-        this.index = this.index === 0? 1 : 0; // We only need a 2 index array.
-      }   
-      
-
+        this.operation = input;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+      }
     } else {
-      if(this.side[this.index] === 0)
-        this.side[this.index] = (this.side[this.index] * 10) + input;
-      else
-        this.side[this.index] = input;
+      this.currentOperand = this.currentOperand + input;
     }
-
   }
 
-  initialize() {
+  calculate() {
+    
+    if(this.previousOperand === '' || this.currentOperand === '') return;
 
-    this.value = 0;
-    this.side = [0, 0];
-    this.index = 0;
-
-  }
-
-  calculate(operateOn) {
-
-    switch(operateOn) {
+    switch(this.operation) {
+      
       case '+': {
-        this.side[0] = this.side[0] + this.side[1];
-        break;
+        this.displayValue = this.currentOperand + this.previousOperand;
       }
 
-      case '-': {
-        this.side[0] = this.side[0] - this.side[1];
-        break;
-      }
-
-      case '*': {
-        this.side[0] = this.side[0] * this.side[1];
-        break;
-      }
     }
-
-    return this.side[0];
-
   }
 
-
-}
-
-export enum ButtonType {
-  NUMBER = 'NUMBER',
-  OPERATOR = 'OPERATOR',
-  CLEAR = 'CLEAR'
 }
